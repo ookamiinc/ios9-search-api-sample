26dce3d8badafe68728c4f1af4f61e251acead51
Masayuki Ono <mono0926@gmail.com>
2015-06-17 19:08:26 +0900

    Add plane project

---
+++ ios9-search-api-sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios9-search-api-sample
+//
+//  Created by mono on 6/17/15.
+//  Copyright © 2015 ookami, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ios9-search-api-sampleTests/ios9_search_api_sampleTests.swift
@@ -0,0 +1,35 @@
+//
+//  ios9_search_api_sampleTests.swift
+//  ios9-search-api-sampleTests
+//
+//  Created by mono on 6/17/15.
+//  Copyright © 2015 ookami, Inc. All rights reserved.
+//
+
+import XCTest
+
+class ios9_search_api_sampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
